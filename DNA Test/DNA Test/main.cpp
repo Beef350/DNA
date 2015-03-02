46708467e3d378599cbbb9bdf127892d49f3d12c
Jacoby Kang <JacobKang@stmarksschool.org>
2015-03-02 09:27:52 -0500

    create first project

---
+++ DNA Test/DNA Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DNA Test
+//
+//  Created by Jacoby Kang on 3/2/15.
+//  Copyright (c) 2015 Jacoby Kang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
